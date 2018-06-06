3a34c33ddfd493bd5c832df14ec320d460d52477
zhonghua <zhu.zhonghua@yahoo.com>
2018-06-07 02:09:37 +0800

    add log test

--- gts/ngx_stream_gts_module.c
+++ gts/ngx_stream_gts_module.c
@@ -5,7 +5,7 @@
 #include <nginx.h>
 
 typedef struct {
-	ngx_str_t  ed;
+	ngx_str_t  cfg;
 } ngx_stream_gts_srv_conf_t;
 
 static void *ngx_stream_gts_create_srv_conf(ngx_conf_t *cf);
@@ -15,11 +15,11 @@ static void ngx_stream_gts_init_session(ngx_stream_session_t *s);
 
 static ngx_command_t  ngx_stream_gts_commands[] = {	
 	{
-		ngx_string("server"),
-		NGX_STREAM_SRV_CONF|NGX_CONF_ANY,
+		ngx_string("cfg"),
+		NGX_STREAM_MAIN_CONF|NGX_STREAM_SRV_CONF|NGX_CONF_FLAG,
 		ngx_stream_gts_on,
 		NGX_STREAM_SRV_CONF_OFFSET,
-		0,
+		offsetof(ngx_stream_gts_srv_conf_t, cfg),
 		NULL
 	},
 	ngx_null_command
@@ -31,6 +31,10 @@ ngx_stream_gts_on(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 	ngx_stream_core_srv_conf_t  *cscf;
 	cscf = (ngx_stream_core_srv_conf_t*)ngx_stream_conf_get_module_srv_conf(cf, ngx_stream_core_module);
 	cscf->handler = ngx_stream_gts_init_session;
+
+	printf("load gts conf\n");
+
+	printf(cmd->args->elts[1]);
 	
 	return NGX_CONF_OK;
 }
@@ -72,7 +76,8 @@ ngx_stream_gts_create_srv_conf(ngx_conf_t *cf)
 		return NULL;
 	}
 
-	ngx_log_stderr(0, "create srv conf");
+	//ngx_log_stderr(0, "create srv conf");
+	printf("gts create conf\n");
 	//conf->ed = ""
 	return conf;
 }
@@ -83,6 +88,7 @@ ngx_stream_gts_merge_srv_conf(ngx_conf_t *cf, void *parent, void *child)
 	//ngx_stream_gts_srv_conf_t *prev = (ngx_stream_gts_srv_conf_t*)parent;
 	//ngx_stream_gts_srv_conf_t *conf = (ngx_stream_gts_srv_conf_t*)child;
 
+	printf("gts merge srv conf");
 	return NGX_CONF_OK;
 }
 
@@ -97,5 +103,7 @@ ngx_stream_gts_init_session(ngx_stream_session_t *s)
 
 	conf = (ngx_stream_gts_srv_conf_t*)ngx_stream_get_module_srv_conf(s, ngx_stream_gts_module);
 
-	ngx_log_stderr(0, "hello world");	
+	//ngx_log_stderr(0, "hello world");
+	printf("hello world\n");
+	printf("init sessioin\n");
 }
